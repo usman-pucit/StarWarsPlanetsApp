052b4e5d88652f816b689c57b90948b93e1e76ba
Muhammad Usman <usmans1020@gmail.com>
2025-05-01 12:51:13 +0200

    chore: Add initial project structure.

---
+++ StarWarsPlanetsApp/StarWarsPlanetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsPlanetsAppApp.swift
+//  StarWarsPlanetsApp
+//
+//  Created by Usman on 01.05.25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsPlanetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarWarsPlanetsAppTests/StarWarsPlanetsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsPlanetsAppTests.swift
+//  StarWarsPlanetsAppTests
+//
+//  Created by Usman on 01.05.25.
+//
+
+import Testing
+@testable import StarWarsPlanetsApp
+
+struct StarWarsPlanetsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
