fc3f0264a29b571e70df70897f914e99e2cdca2d
Muhammad Usman <usmans1020@gmail.com>
2025-05-05 02:15:33 +0200

    fix: Correct typo in StarWarsPlanetsApp.swift file name.

--- StarWarsPlanetsApp/Application/StarWarsPlanetsApp.swift
+++ StarWarsPlanetsApp/Application/StarWarsPlanetsApp.swift
@@ -1,5 +1,5 @@
 //
-//  StarWarsPlanetsAppApp.swift
+//  StarWarsPlanetsApp.swift
 //  StarWarsPlanetsApp
 //
 //  Created by Usman on 01.05.25.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct StarWarsPlanetsAppApp: App {
+struct StarWarsPlanetsApp: App {
     var body: some Scene {
         WindowGroup {
             PlanetsListView()
